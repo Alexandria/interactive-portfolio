b225aec0d2e47f7d222931e3d7856711ceea7625
Lex <lexluka1031@gmail.com>
2024-04-23 18:45:07 -0500

    Center game and scale fit

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import Example from "./scenes/Example";
 import Preloader from "./scenes/Preloader";
 import Game from "./scenes/Game";
 
+// Responzise Game mode
+// https://stackoverflow.com/questions/51518818/how-to-make-canvas-responsive-using-phaser-3
+// https://www.emanueleferonato.com/2018/02/16/how-to-scale-your-html5-games-if-your-framework-does-not-feature-a-scale-manager-or-if-you-do-not-use-any-framework/
+
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.CANVAS,
   scale: {
     mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1000,
+    height: 350,
   },
-  width: 1000,
-  height: 350,
   pixelArt: true, // Makes the pixel art have clear resolution
   physics: {
     default: "arcade",
@@ -21,5 +26,11 @@ const config: Phaser.Types.Core.GameConfig = {
   },
   scene: [Preloader, Game],
 };
+const canvas = document.getElementsByTagName("canvas");
+const gameContainer = document.createElement("div");
+//gameContainer.appendChild(canvas);
+window.onload = function () {
+  const game = new Phaser.Game(config);
+};
 
-export default new Phaser.Game(config);
+//export default new Phaser.Game(config);
