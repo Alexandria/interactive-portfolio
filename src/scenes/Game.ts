a330f4962779673570d10b5e5ef9a9e0d1c1cd57
Lex <lexluka1031@gmail.com>
2024-05-05 14:34:31 -0500

    Toggle joystick if not mobile

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -10,6 +10,23 @@ import Preloader from "./Preloader";
 
 // https://newdocs.phaser.io/docs/3.80.0/Phaser.Animations.AnimationManager#create
 
+const isMobile = () => {
+  // https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a,
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4),
+      )
+    )
+      check = true;
+  })(navigator.userAgent);
+  return check;
+};
+
 export default class Game extends Phaser.Scene {
   private player: Player;
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
@@ -22,6 +39,7 @@ export default class Game extends Phaser.Scene {
   private candyHadPic: Picture;
   private cursedPic: Picture;
   private resizeScale: number;
+  private isMobile: boolean;
 
   constructor() {
     // https://stackoverflow.com/questions/52864250/what-is-the-function-of-super-in-phaser-frameworks
@@ -32,6 +50,7 @@ export default class Game extends Phaser.Scene {
 
   create() {
     this.resizeScale = SCALE;
+    this.isMobile = false;
     const speed = 100;
     const yPosition = 100;
     const safeAreaX =
@@ -53,8 +72,6 @@ export default class Game extends Phaser.Scene {
       width: worldMapHtWd.width,
       height: worldMapHtWd.height,
     };
-
-    console.log("world specs", world);
 
     const playerPos = worldMap.getMarkerPositionByName("player")!;
 
@@ -192,9 +209,17 @@ export default class Game extends Phaser.Scene {
       dir: "4dir",
     });
     this.joyCursors = this.joyStick.createCursorKeys();
-    console.log("joystick xy", this.joyStick.x, ": ", this.joyStick.y);
 
     const resize = (displaySize?: any, gameSize?: any) => {
+      this.isMobile = isMobile();
+      if (!this.isMobile) {
+        this.joyStick.setVisible(false);
+      } else {
+        this.joyStick.setVisible(true);
+      }
+      console.log("isMobile", this.isMobile);
+      // handle landscape versus Portrait
+
       const newScale = Math.max(this.cameras.main.height / 200, 1);
 
       // update position of safe area
@@ -254,8 +279,6 @@ export default class Game extends Phaser.Scene {
         this.joyStick.y = joyStickPosY;
       }
 
-      console.log("window inner width", window.innerWidth);
-
       // // adjust the controls section
       // this.joyStick.x = joyStickPosX;
       // this.joyStick.y = joyStickPosY;
@@ -281,10 +304,7 @@ export default class Game extends Phaser.Scene {
 
     this.scale.on("resize", (gameSize, baseSize, displaySize, resolution) => {
       this.cameras.resize(gameSize.width, gameSize.height);
-      console.log("gameSize", gameSize.height);
-      console.log("baseSize", baseSize);
-      console.log("resolution", resolution);
-      console.log("✨ safe area x", safeArea.x);
+
       resize(displaySize, gameSize);
     });
 
@@ -299,7 +319,7 @@ export default class Game extends Phaser.Scene {
   playerPicInteraction(pic: Picture) {
     // if player is with in the object hitbox AND presses
     // the space bar. Then open an separate link
-    console.log("Start player interaction!");
+
     if (pic.getLink()) {
       window.open(pic.getLink());
     }
