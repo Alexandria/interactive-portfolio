e88f7b3e598cda724ea3ca3fcb319ce7f15084f0
Lex <lexluka1031@gmail.com>
2024-06-09 16:28:05 -0500

    Clean up functions and set picture interaction on pointer click

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,32 +1,14 @@
-import Phaser, { LEFT, Scale } from "phaser";
+import Phaser from "phaser";
 import Player from "../components/Player";
-import { debugDraw } from "../utils/debugDraw";
 import WorldMap from "../components/WorldMap";
 import Picture from "../components/Picture";
 import { BreakPoints, PictureNames } from "../types";
 import VirtualJoystick from "phaser3-rex-plugins/plugins/virtualjoystick.js";
 import { SCALE } from "../main";
-import Preloader from "./Preloader";
 import { portraitBreakPoints, landscapeBreakPoints } from "../utils/fixtures";
+import { isMobile } from "../utils/isMobile";
 
 // https://newdocs.phaser.io/docs/3.80.0/Phaser.Animations.AnimationManager#create
-
-const isMobile = () => {
-  // https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
-  let check = false;
-  (function (a) {
-    if (
-      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
-        a,
-      ) ||
-      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-        a.substr(0, 4),
-      )
-    )
-      check = true;
-  })(navigator.userAgent);
-  return check;
-};
 
 type Orientation = "Portrait" | "Landscape" | "Web";
 
@@ -34,7 +16,6 @@ export default class Game extends Phaser.Scene {
   private player: Player;
   private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
   private wasdKeys?;
-  private joyCursors?;
   private joyStick;
   private matchThreePic: Picture;
   private fnafPic: Picture;
@@ -50,7 +31,61 @@ export default class Game extends Phaser.Scene {
     super("game");
   }
 
-  preload() {}
+  createAllPictures(picPos, yPosition) {
+    this.matchThreePic = new Picture(
+      {
+        scene: this,
+        x: picPos.matchThreePosition.x! * SCALE,
+        y: picPos.matchThreePosition.y! * SCALE + yPosition,
+        key: "sign",
+      },
+      this.resizeScale,
+      "https://match-three-game.netlify.app/",
+    );
+
+    this.fnafPic = new Picture(
+      {
+        scene: this,
+        x: picPos.fnafPosition.x! * this.resizeScale,
+        y: picPos.fnafPosition.y! * this.resizeScale + yPosition,
+        key: "sign",
+      },
+      this.resizeScale,
+      "https://www.pixilart.com/art/fnaf-favs-a5042ea5c957897",
+    );
+
+    this.inTheWoodsPic = new Picture(
+      {
+        scene: this,
+        x: picPos.inTheWoodsPos.x! * this.resizeScale,
+        y: picPos.inTheWoodsPos.y! * this.resizeScale + yPosition,
+        key: "sign",
+      },
+      this.resizeScale,
+      "https://www.pixilart.com/art/rainy-forest-596e36029c4f600",
+    );
+
+    this.candyHadPic = new Picture(
+      {
+        scene: this,
+        x: picPos.candyHagDashPos.x! * SCALE,
+        y: picPos.candyHagDashPos.y! * SCALE + yPosition,
+        key: "sign",
+      },
+      this.resizeScale,
+      "https://coffeeghoststudios.itch.io/candy-hag-dash",
+    );
+
+    this.cursedPic = new Picture(
+      {
+        scene: this,
+        x: picPos.cursedPicPos.x! * this.resizeScale,
+        y: picPos.cursedPicPos.y! * this.resizeScale + yPosition,
+        key: "sign",
+      },
+      this.resizeScale,
+    );
+  }
 
   create() {
     this.resizeScale = SCALE;
@@ -59,8 +94,6 @@ export default class Game extends Phaser.Scene {
     const yPosition = 100;
     const safeAreaX = window.innerWidth / 3;
     const safeAreaY = yPosition + 20;
-    const safeAreaWidth = 500;
-    const safeAreaHeight = 400;
     this.cameras.main.height / 2.5 - +this.game.config.height / 4.5 - 5;
     const joyStickPosX = safeAreaX;
     const joyStickPosY = safeAreaY + 500;
@@ -81,7 +114,7 @@ export default class Game extends Phaser.Scene {
 
     const playerPos = worldMap.getMarkerPositionByName("player")!;
 
-    //worldMap.worldDebug();
+    worldMap.worldDebug();
 
     this.cursors = this.input.keyboard?.createCursorKeys();
 
@@ -101,63 +134,21 @@ export default class Game extends Phaser.Scene {
 
     this.wasdKeys = this.input.keyboard?.addKeys("W,S,A,D");
 
-    this.matchThreePic = new Picture(
+    this.createAllPictures(
       {
-        scene: this,
-        x: matchThreePosition.x! * SCALE,
-        y: matchThreePosition.y! * SCALE + yPosition,
-        key: "sign",
+        matchThreePosition,
+        fnafPosition,
+        inTheWoodsPos,
+        candyHagDashPos,
+        cursedPicPos,
       },
-      this.resizeScale,
-      "https://match-three-game.netlify.app/",
+      yPosition,
     );
 
-    this.fnafPic = new Picture(
-      {
-        scene: this,
-        x: fnafPosition.x! * this.resizeScale,
-        y: fnafPosition.y! * this.resizeScale + yPosition,
-        key: "sign",
-      },
-      this.resizeScale,
-      "https://www.pixilart.com/art/fnaf-favs-a5042ea5c957897",
-    );
-
-    this.inTheWoodsPic = new Picture(
-      {
-        scene: this,
-        x: inTheWoodsPos.x! * this.resizeScale,
-        y: inTheWoodsPos.y! * this.resizeScale + yPosition,
-        key: "sign",
-      },
-      this.resizeScale,
-      "https://www.pixilart.com/art/rainy-forest-596e36029c4f600",
-    );
-
-    this.candyHadPic = new Picture(
-      {
-        scene: this,
-        x: candyHagDashPos.x! * SCALE,
-        y: candyHagDashPos.y! * SCALE + yPosition,
-        key: "sign",
-      },
-      this.resizeScale,
-      "https://coffeeghoststudios.itch.io/candy-hag-dash",
-    );
-
-    this.cursedPic = new Picture(
-      {
-        scene: this,
-        x: cursedPicPos.x! * this.resizeScale,
-        y: cursedPicPos.y! * this.resizeScale + yPosition,
-        key: "sign",
-      },
-      this.resizeScale,
-    );
     this.player = new Player(
       {
         scene: this,
-        x: playerPos.x! * this.resizeScale,
+        x: playerPos.x! * this.resizeScale - 100,
         y: playerPos.y! * this.resizeScale + yPosition,
         key: "character",
       },
@@ -167,11 +158,36 @@ export default class Game extends Phaser.Scene {
 
     worldMap.addCollision(this.player);
 
-    this.matchThreePic.addCollision(this.player);
-    this.fnafPic.addCollision(this.player);
-    this.inTheWoodsPic.addCollision(this.player);
-    this.candyHadPic.addCollision(this.player);
-    this.cursedPic.addCollision(this.player);
+    this.matchThreePic.on("pointerdown", () => {
+      if (this.matchThreePic.isPlayerPostionNear(this.player)) {
+        console.log("position is true matchthree");
+        this.playerPicInteraction(this.matchThreePic);
+      }
+    });
+    this.fnafPic.on("pointerdown", () => {
+      if (this.fnafPic.isPlayerPostionNear(this.player)) {
+        console.log("position is true fnaf");
+        this.playerPicInteraction(this.fnafPic);
+      }
+    });
+    this.inTheWoodsPic.on("pointerdown", () => {
+      if (this.inTheWoodsPic.isPlayerPostionNear(this.player)) {
+        console.log("position is true inthewoods");
+        this.playerPicInteraction(this.inTheWoodsPic);
+      }
+    });
+    this.candyHadPic.on("pointerdown", () => {
+      if (this.candyHadPic.isPlayerPostionNear(this.player)) {
+        console.log("position is true candyHag");
+        this.playerPicInteraction(this.candyHadPic);
+      }
+    });
+    this.cursedPic.on("pointerdown", () => {
+      if (this.cursedPic.isPlayerPostionNear(this.player)) {
+        console.log("position is true cursed pic");
+        this.playerPicInteraction(this.cursedPic);
+      }
+    });
 
     // Camera
     const cam = this.cameras.main;
@@ -187,24 +203,6 @@ export default class Game extends Phaser.Scene {
     );
     //this.physics.world.setBounds(0, 0, world.width, world.height);
     cam.startFollow(this.player, true);
-    //cam.zoom = 0.3;
-
-    // EndCamera
-
-    // draw safe area
-    // let safeArea = this.add
-    //   .rectangle(
-    //     safeAreaX,
-    //     safeAreaY,
-    //     safeAreaWidth,
-    //     safeAreaHeight,
-    //     0xff00ff,
-    //     0.08,
-    //   )
-    //   .setStrokeStyle(4, 0xff00ff, 0.25)
-    //   .setOrigin(0)
-    //   .setDepth(2)
-    //   .setScrollFactor(0);
 
     this.joyStick = new VirtualJoystick(this, {
       x: joyStickPosX,
@@ -214,7 +212,6 @@ export default class Game extends Phaser.Scene {
       radius: 50,
       dir: "4dir",
     });
-    this.joyCursors = this.joyStick.createCursorKeys();
 
     const resize = () => {
       this.isMobile = isMobile();
@@ -476,18 +473,23 @@ export default class Game extends Phaser.Scene {
 
   cursedPicInteraction() {
     // enable dresser animation opening
+    console.log("Boo!");
   }
 
   playerPicInteraction(pic: Picture) {
-    // if player is with in the object hitbox AND presses
-    // the space bar. Then open an separate link
-
     if (pic.getLink()) {
       window.open(pic.getLink());
     }
   }
 
   update(time: number, delta: number): void {
+    const touching = !this.player.body?.touching.none;
+    const wasTouching = !this.player.body?.wasTouching.none;
+    const spaceIsDown = this.cursors?.space.isDown;
+
+    if (touching && !wasTouching) this.player.emit("overlapstart");
+    if (!touching && wasTouching) this.player.emit("overlapend");
+
     // Player Movements
     this.player.stopMoving();
     if (this.cursors && this.cursors.left.isDown) {
@@ -519,35 +521,5 @@ export default class Game extends Phaser.Scene {
     } else if (this.cursors && this.joyStick.down) {
       this.player.moveDown(delta);
     }
-
-    //End Player Movement
-
-    if (this.matchThreePic.isInInteractZone() && this.cursors?.space.isDown) {
-      // Perhaps also check if player is not moving?
-      //Handle Player interaction
-      this.playerPicInteraction(this.matchThreePic);
-    }
-    if (this.fnafPic.isInInteractZone() && this.cursors?.space.isDown) {
-      // Perhaps also check if player is not moving?
-      //Handle Player interaction
-      this.playerPicInteraction(this.fnafPic);
-    }
-
-    if (this.inTheWoodsPic.isInInteractZone() && this.cursors?.space.isDown) {
-      // Perhaps also check if player is not moving?
-      //Handle Player interaction
-      this.playerPicInteraction(this.inTheWoodsPic);
-    }
-
-    if (this.candyHadPic.isInInteractZone() && this.cursors?.space.isDown) {
-      // Perhaps also check if player is not moving?
-      //Handle Player interaction
-      this.playerPicInteraction(this.candyHadPic);
-    }
-
-    this.candyHadPic.setInInteractZone(false);
-    this.inTheWoodsPic.setInInteractZone(false);
-    this.fnafPic.setInInteractZone(false);
-    this.matchThreePic.setInInteractZone(false);
   }
 }
